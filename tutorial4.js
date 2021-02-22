a857b872bf5824ee91e77e23802bd7763302f3e9
ttachiki <ttachiki@yahoo-corp.jp>
2021-02-22 18:42:41 +0900

    update

--- tutorial4.js
+++ tutorial4.js
@@ -1,1 +1,22 @@
-(function ())
+(function () {
+    "use strict"
+    kintone.events.on('app.record.detail.show', function(event) {
+        var myMenuButton = document.createElement('button');
+        myMenuButton.id = 'my_menu_button';
+        myMenuButton.innerText = 'ボタン';
+        myMenuButton.onclick = function() {
+            var mySpaceField = kintone.app.record.getSpaceElement('my_space_field');
+            mySpaceField.parentNode.style.width = '400px';
+
+            var updatedAt = '';
+            var rec = kintone.app.record.get();
+            if (rec) {
+                updatedAt = rec.record.更新日時.value;
+            }
+            var time = new Date(updatedAt);
+            //kintone.app.record.getSpaceElement('my_space_field').innerText = updatedAt;
+            kintone.app.record.getSpaceElement('my_space_field').innerText = time.toLocaleString();
+        };
+        kintone.app.record.getHeaderMenuSpaceElement().appendChild(myMenuButton);
+    });
+})();
