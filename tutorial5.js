5ceabaacb6fa7715900be767648f11990a587057
ttachiki <ttachiki@yahoo-corp.jp>
2021-02-22 19:38:03 +0900

    update

--- tutorial5.js
+++ tutorial5.js
@@ -15,25 +15,40 @@
         myListTable.style.width = '600px';
 
         var records = event.records;
+        var signalColorParts = kintone.app.getFieldElements('信号の色');
 
         for (var i = 0; i < records.length; i++) {
             var record = records[i];
 
+            var color = records[i]['信号の色'].value;
+
+            var part = signalColorParts[i];
+            part.style.fontWeight = 'bold';
+            part.style.color = '#ffffff';
+
+            if (color ==='赤') {
+                part.style.backgorundColor = '#ff0000';
+            } else if (color === '青' ) {
+                part.style.backgroundColor = '#0000ff';
+            } else if (color === '黄') {
+                part.style.backgroundColor = '#ffd700';
+            }
+
             var myRecordTd1 = document.createElement('td');
             myRecordTd1.id = 'my_record_td_' + i + '_1';
             myRecordTd1.style.border = '1px solid';
-            myRecordTd1.innerText = record.レコード番号.value;
+            myRecordTd1.innerText = record['レコード番号'].value;
             // 信号の色
             var myRecordTd2 = document.createElement('td');
             myRecordTd2.id = 'my_record_td_' + i + '_2';
             myRecordTd2.style.border = '1px solid';
-            myRecordTd2.innerText = record.信号の色.value;
+            myRecordTd2.innerText = record['信号の色'].value;
 
             // 作成日時k
             var myRecordTd3 = document.createElement('td');
             myRecordTd3.id = 'my_record_td_' + i + '_3';
             myRecordTd3.style.border = '1px solid';
-            myRecordTd3.innerText = new Date(record.作成日時.value).toLocaleString();
+            myRecordTd3.innerText = new Date(record['作成日時'].value).toLocaleString();
 
             var myRecordTr = document.createElement('tr');
             myRecordTr.id = 'my_record_tr_' + i;
